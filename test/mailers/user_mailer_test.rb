8e6bffe305ee3d91c4a2f74f337998f9a13a4ea6
derrick <suarez.derrick@gmail.com>
2018-04-13 00:25:43 -0500

    password resets functioning; need to add tests and confirm expiration check and functionality in production

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -5,7 +5,7 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:admin_user)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
-    assert_equal "Account activation", mail.subject
+    assert_equal "Account Activation", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@ritter.com"], mail.from
     assert_match user.name, mail.body.encoded
@@ -13,4 +13,14 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match CGI.escape(user.email), mail.body.encoded
   end
 
+  test "password_reset" do
+    user = users(:admin_user)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal "Password Reset", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@ritter.com"], mail.from
+    assert_match user.name, mail.body.encoded
+    assert_match user.reset_token, mail.body.encoded
+    assert_match CGI.escape(user.email), mail.body.encoded  end
 end
